8e48b73d39c98088a820097892022a3fc7750fc7
Mathias Rendon <mathiassaid7@gmail.com>
2026-01-27 18:44:55 -0500

    feat: add async handler and remove try/catch blocks

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,28 +1,24 @@
 import { AppError } from "@/utils/AppError.js";
 import type { NextFunction, Request, Response } from "express";
 import * as AuthService from "@/services/auth.services.js";
+import { asyncHandler } from "@/handlers/asyncHandler.js";
 
 export class AuthController {
-  static async register(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { email, password, name } = req.body;
+  static register = asyncHandler(async (req: Request, res: Response) => {
+    const { email, password, name } = req.body;
 
-      if (!email || !email.trim() || !password || !password.trim()) throw new AppError("Email or password missing", 400);
+    if (!email || !email.trim() || !password || !password.trim()) throw new AppError("Email or password missing", 400);
 
-      if (!(email as string).match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) throw new AppError("Enter a valid email", 400);
+    if (!(email as string).match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) throw new AppError("Enter a valid email", 400);
 
-      if (password.length < 8) throw new AppError("Please enter a stronger password", 400);
+    if (password.length < 8) throw new AppError("Please enter a stronger password", 400);
 
-      const newUser = await AuthService.register({ email, password, name });
+    const newUser = await AuthService.register({ email, password, name });
 
-      res.status(201).json(newUser);
-    } catch (error) {
-      next(error);
-    }
-  }
+    res.status(201).json(newUser);
+  })
 
-  static async login(req: Request, res: Response, next: NextFunction) {
-    try {
+  static login = asyncHandler(async (req: Request, res: Response) => {
       const { email, password } = req.body;
 
       if (!email || !email.trim() || !password || !password.trim()) throw new AppError("Email or password missing", 400);
@@ -30,8 +26,5 @@ export class AuthController {
       const credentials = await AuthService.login(email, password);
 
       res.status(200).json(credentials);
-    } catch (error) {
-      next(error);
-    }
-  }
+  })
 }
